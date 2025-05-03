c608c93b9120f5fbc294ec7157097f20299a267a
Du Li <dl38986@my.utexas.edu>
2025-05-03 07:59:29 +0000

    init h1

---
+++ models/base/sample.py
@@ -0,0 +1,5 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-1.7B-Instruct")
+model = AutoModelForCausalLM.from_pretrained("HuggingFaceTB/SmolLM2-1.7B-Instruct")
